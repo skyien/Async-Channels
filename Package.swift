a768e693141e8bd2fa4dd65a39248339e6b6f77a
yuranov <V.yuranov@gmail.com>
2026-05-26 11:02:02 +0300

    wip

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 6.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,18 +6,17 @@ import PackageDescription
 let package = Package(
     name: "AsyncChannels",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v10_15)
+        .macOS(.v26)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AsyncChannels",
-            targets: ["AsyncChannels"]),
+            targets: ["AsyncChannels"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/apple/swift-collections", from: "1.1.0")
+        .package(url: "https://github.com/apple/swift-collections", from: "1.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
